f4662e1952d0e3ba02bb05e466c4758e3f476cac
Aditya Ridharrahman <adityaridharrahman@gmail.com>
2022-10-04 15:38:30 +0700

    latihan

---
+++ src/main/kotlin/Conditional.kt
@@ -0,0 +1,21 @@
+fun main () {
+
+    val isSamboGuilty = true
+    val isFalse = false
+
+    if (isSamboGuilty) println("He will be jailed") else println("Something wrong with this country")
+
+    val lamaTahanan = if (isSamboGuilty) 50 else 0
+
+    println(lamaTahanan)
+
+
+    val num = 4
+
+    val result = when {
+        num > 5 && num < 7-> "Positive"
+        num > 10 -> "Negati ve"
+        else -> "out of bound"
+    }
+    println(result)
+}
